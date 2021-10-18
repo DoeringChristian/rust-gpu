a7cc8fb15e430179919c51fcb7ef0288152da77c
Ashley Ruglys <ashley.ruglys@gmail.com>
2021-10-18 04:28:40 -0700

    Update example

--- examples/shaders/sky-shader/src/lib.rs
+++ examples/shaders/sky-shader/src/lib.rs
@@ -176,13 +176,13 @@ pub fn main_vs(#[spirv(vertex_index)] vert_idx: i32, #[spirv(position)] builtin_
     *builtin_pos = pos.extend(0.0).extend(1.0);
 
     unsafe {
-        spirv_std::macros::printfln!("uv for vert index %i: vec2(%1.2v2f)", vert_idx, uv);
+        spirv_std::macros::debug_printfln!(r#"uv for vert index "%i": vec2(%1.2v2f)"#, vert_idx, uv);
     }
 
     if vert_idx == 0 {
         // Insert a newline to leave a gap between each draw call.
         unsafe {
-            spirv_std::macros::printfln!();
+            spirv_std::macros::debug_printfln!();
         }
     }
 }
